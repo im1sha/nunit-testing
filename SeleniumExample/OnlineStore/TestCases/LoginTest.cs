c7b3eb4b41399850509f6edf1b845b17bc29232d
im1sha <vkporsche@ya.ru>
2018-11-09 11:02:55 +0300

    usage example implemented

---
+++ SeleniumExample/OnlineStore/TestCases/LoginTest.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using OnlineStore.PageObjects;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineStore.TestCases
+{
+    class LoginTest
+    {
+        [Test]
+        public void Test()
+        {
+            IWebDriver driver = new ChromeDriver
+            {
+                Url = "http://www.store.demoqa.com"
+            };
+
+
+            var homePage = new HomePage();
+            PageFactory.InitElements(driver, homePage);
+            homePage.MyAccount.Click();
+
+            var loginPage = new LoginPage();
+            PageFactory.InitElements(driver, loginPage);
+            loginPage.UserName.SendKeys("TestUser_1");
+            loginPage.Password.SendKeys("Test@123");
+            loginPage.Submit.Submit();
+
+            driver.Quit();
+        }
+    }
+}
---
+++ SeleniumExample/SeleniumExample/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var t = new BasicTest();
+            t.Initiaize();
+            t.Test();
+            t.Close();
+        }
+    }
+}
