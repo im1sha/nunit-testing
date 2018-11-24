91818eeee8a96850124174e49da18bb3d23ad77e
im1sha <vkporsche@ya.ru>
2018-11-24 04:42:28 +0300

    AddItemWithMultiselect implemented

--- SeleniumTesting/LiverpoolMuseumTesting/Shop.cs
+++ SeleniumTesting/LiverpoolMuseumTesting/Shop.cs
@@ -1,6 +1,7 @@
 ﻿using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -44,6 +45,43 @@ namespace LiverpoolMuseumTesting
 
             Assert.AreEqual(elements.Count, 1);
             Assert.AreNotEqual(elements, null);
+        }
+
+
+        [Test]
+        public void AddItemWithMultiselect()
+        {
+            driver.Url = "http://www.liverpoolmuseums.org.uk/onlineshop/gifts/liverpool-gifts/red-ceramic-liverbird.aspx";
+
+            driver.Manage().Timeouts().ImplicitWait = new TimeSpan(0, 0, 10);
+
+            string path = "/html/body/form[@id='form1']/div[@class='wrapper home']/div[@class='wrapper-inner is-shop']/div[@class='content page']/div[@class='container-fluid']/div[@class='row-fluid']/div[@class='span9']/div[@class='panel']/div[@id='main-content']/div[@id='productHolder']/div[@class='shop-item-detail']/div[@id='productVariations']/div[@id='variationsUpdatePnl']/input[@id='addToCartButton']";
+
+
+            List<IWebElement> buttonList = null;
+            try
+            {
+                buttonList = driver.FindElements(By.XPath(path)).ToList();
+            }
+            catch 
+            {
+
+            }
+
+            Assert.AreEqual(buttonList[0].Enabled, false);
+            Assert.AreEqual(buttonList.Count, 1);
+
+            string pathToDroplist = "/html/body/form[@id='form1']/div[@class='wrapper home']/div[@class='wrapper-inner is-shop']/div[@class='content page']/div[@class='container-fluid']/div[@class='row-fluid']/div[@class='span9']/div[@class='panel']/div[@id='main-content']/div[@id='productHolder']/div[@class='shop-item-detail']/div[@id='productVariations']/div[@id='variationsUpdatePnl']/div[@id='colourVariationsPnl']/select[@id='colourVariationsDDL']";
+            IWebElement element = driver.FindElement(By.XPath(pathToDroplist));
+            SelectElement select = new SelectElement(element);
+            select.SelectByText("red");
+
+            var wait = new WebDriverWait(driver, TimeSpan.FromMinutes(1));
+            var clickableElement = wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath(path)));
+
+            Assert.AreEqual(clickableElement.Enabled, true);
+            Assert.AreEqual(buttonList.Count, 1);
+
         }
 
 
